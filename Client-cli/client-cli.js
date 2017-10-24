2bdc4dec32561128112281a12e038c0b3f986840
Victor Carvalho <victor.blq@gmail.com>
2017-10-24 13:58:02 -0200

    correçoes script cli

--- Client-cli/client-cli.js
+++ Client-cli/client-cli.js
@@ -7,7 +7,7 @@ admin.initializeApp({
     databaseURL: "https://qualygame.firebaseio.com/"
 });
 
-console.log("Qualygame Client-CLI Started \n");
+console.log("Qualygame Client-CLI Started \r\n");
 //argv[0] is the nodejs home 
 //argv[1] is the script directory (full path)
 let hash = process.argv[2];
@@ -21,7 +21,7 @@ admin.database().ref("/users")
 .equalTo(userEmail)
 .once("child_added", ( result ) => {
     var user = result.val();
-    console.log("User found! \n");    
+    console.log("User found! \r\n");    
     findTeamInteration(user);
 });
 
@@ -48,23 +48,26 @@ function findTeamInteration(user)
         {
             let activeIteration = null;
 
-            foundTeam.projects.find( ( projectToFind ) => {
-                for( let i = 0; i < foundTeam.projects.length; i++ )
+            if(foundTeam.projects != null)
+            {
+                for(let projectKey in foundTeam.projects)
                 {
-                    for( let key in foundTeam.projects[i].iterations)
+                    for( let iterationKey in foundTeam.projects[projectKey].iterations)
                     {
-                        if(foundTeam.projects[i].iterations[key].status == 1)
+                        if(foundTeam.projects[projectKey].iterations[iterationKey].status == 1)
                         {
-                            activeIteration = key;
-                            return true;
+                            activeIteration = iterationKey;
+                            break;
                         }
                     }
+
+                    break;
                 }
-            });
+            }
 
             if(activeIteration != null)
             {
-                console.log("Iteration found!");
+                console.log("Iteration found! \r\n");
                 prepareCommitedArtifacts(user, activeIteration);
             }
         }
@@ -99,7 +102,7 @@ function prepareCommitedArtifacts(user, activeIteration)
             }
         };
 
-        console.log("Commit created!");
+        console.log("Commit created!\r\n");
         persistCommit(user, activeIteration, commitedArtifacts);
 
     });
@@ -122,15 +125,15 @@ function persistCommit(user, activeIteration, commitedArtifacts)
         admin.database().ref("/commits/"+commit.hash+"/commitedArtifacts")
         .set(commitedArtifacts)
         .then( (result) => {
-            if(commitedArtifacts.length == 0)
+            if(commitedArtifacts.length > 0)
             {   
-                persistCommitPontuation(user, commit, "COMMIT_WITHOUT_ARTIFACT");
-                console.log("Persisting commit with positive pontuation!");
+                console.log("Persisting commit with positive pontuation!\r\n");
+                persistCommitPontuation(user, commit, "COMMIT");
             }
             else
             {
-                console.log("Persisting commit with negative pontuation!");
-                persistCommitPontuation(user, commit, "COMMIT");
+                console.log("Persisting commit with negative pontuation!\r\n");
+                persistCommitPontuation(user, commit, "COMMIT_WITHOUT_ARTIFACT");
             }
         })
         .catch( ( exception ) => {
@@ -164,7 +167,7 @@ function persistCommitPontuation(user, commit, actionName)
         admin.database().ref("/users/"+user.nickname+"/pontuation")
         .push(pontuation)
         .then( ( result ) => {
-            console.log("Commit and pontuation persisted! Hash: "+commit.hash);
+            console.log("Commit and pontuation persisted! Hash: "+commit.hash+"\r\n");
             process.exit(0);
         });
     });
